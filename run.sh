3d1e2aa3f67c78f1caa36c48ea8d13d4a312fa8c
Amal <amalbenny@duck.com>
2025-10-09 23:09:11 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,11 @@ cd "$(dirname "$0")"
 
 # Create virtual environment if it doesn't exist
 if [ ! -d ".venv" ]; then
-	python3 -m venv .venv
+    python3 -m venv .venv || { echo "Failed to create virtual environment"; exit 1; }
+    source .venv/bin/activate || { echo "Failed to activate virtual environment"; exit 1; }
+    python3 -m pip install -r requirements.txt || { echo "Failed to install requirements"; exit 1; }
+else
+    source .venv/bin/activate || { echo "Failed to activate virtual environment"; exit 1; }
 fi
 
-source .venv/bin/activate
 python3 __main__.py
